d3822b17665c81790e6fc5bcefccb1e6408f775e
Jacob Bowen <jacobbowen2661@gmail.com>
2023-01-02 16:19:05 -0600

    Finished Recursive

--- Fib.py
+++ Fib.py
@@ -14,16 +14,33 @@ Fib(n) = Fib(n-2) + Fib(n-1)
 Sequence: 1, 1, 2, 3, 5, 8, 13, 21,…
 """
 
+
 #GLOABL VARIABLE FOR STARTING FIBONACCI SEQEUNCE
 F0 = 0
 F1 = 1
 
+
 #How many iterations to run
 iteration_amount = int(input("How many iterations of the Fibonacci Sequence would you like to do? "))
+fib_sequence = []
+
+
+#Do Recursively
+fib_sequence.clear()
+def recursive_fib(iteration_amount) -> int:
+    if iteration_amount in {0, 1}:
+        return iteration_amount
+    else:
+        return recursive_fib(iteration_amount - 1) + recursive_fib(iteration_amount - 2)
+
+for i in range(iteration_amount):
+    fib_sequence.append(recursive_fib(i))
+
+print(fib_sequence)
 
 
 #Do Iteratively
-fib_sequence = []
+fib_sequence.clear()
 for i in range(iteration_amount):
     if i == 0:
         fib_sequence.append(F0)
@@ -33,5 +50,3 @@ for i in range(iteration_amount):
         fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
 
 print(fib_sequence)
-
-#Do Recursively
